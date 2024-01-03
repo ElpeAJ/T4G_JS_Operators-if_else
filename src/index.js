6aafcf8ec64267c178c5c6d20ca9497a81db3b9a
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-01-03 14:36:28 -0800

    Pushing all files created

---
+++ src/index.js
@@ -0,0 +1,27 @@
+let x = 10;
+let y = 15;
+
+// Comparison Operators
+console.log(x > y);
+console.log(x == y);
+console.log(x === y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x != y);
+
+// Logical Operators
+
+// If Else Statements
+let username = prompt("What's your name please? ");
+let userAge = prompt("How old are you? ");
+let occupation = prompt("What is your occupation? ");
+
+// if (userAge == 16) {
+//   alert(`Hi ${username}, Happy Sweet Sixteen`);
+// } else {
+//   alert(
+//     `Hi ${username} there is nothing spectacular about your age: ${userAge}, or is there?`
+//   );
+// }
+
+
