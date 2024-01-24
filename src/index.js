7ece37973a586c212ad150bf02ed32a196955134
elpeAJ <elpediaarthurjunior@gmail.com>
2024-01-24 16:57:24 +0000

    Wrote and Commented out JS code in the js file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-let x = 10;
-let y = 15;
+// let x = 10;
+// let y = 15;
 
-// Comparison Operators
-console.log(x > y);
-console.log(x == y);
-console.log(x === y);
-console.log(x >= y);
-console.log(x <= y);
-console.log(x != y);
+// // Comparison Operators
+// console.log(x > y);
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x != y);
 
-// Logical Operators
+// // Logical Operators
 
-// If Else Statements
-let username = prompt("What's your name please? ");
-let userAge = prompt("How old are you? ");
-let occupation = prompt("What is your occupation? ");
+// // If Else Statements
+// let username = prompt("What's your name please? ");
+// let userAge = prompt("How old are you? ");
+// let occupation = prompt("What is your occupation? ");
 
 // if (userAge == 16) {
 //   alert(`Hi ${username}, Happy Sweet Sixteen`);
